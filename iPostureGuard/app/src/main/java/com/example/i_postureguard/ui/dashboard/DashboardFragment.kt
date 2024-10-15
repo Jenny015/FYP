393b6b1a193484de92a1520f8202a793c578672b
LAU Sze Ting (230268178) <230268178@stu.vtc.edu.hk>
2024-10-15 11:30:48 +0800

    add distance detection function, not accurate

--- iPostureGuard/app/src/main/java/com/example/i_postureguard/ui/dashboard/DashboardFragment.kt
+++ iPostureGuard/app/src/main/java/com/example/i_postureguard/ui/dashboard/DashboardFragment.kt
@@ -2,7 +2,11 @@ package com.example.i_postureguard.ui.dashboard
 
 import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.PointF
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +14,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.ToggleButton
 import androidx.annotation.OptIn
+import androidx.camera.camera2.interop.Camera2CameraInfo
+import androidx.camera.camera2.interop.ExperimentalCamera2Interop
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageAnalysis
@@ -19,9 +25,12 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.i_postureguard.databinding.FragmentDashboardBinding
+import com.google.common.util.concurrent.ListenableFuture
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.Face
 import com.google.mlkit.vision.face.FaceDetection
 import com.google.mlkit.vision.face.FaceDetectorOptions
+import com.google.mlkit.vision.face.FaceLandmark
 
 
 const val REQUEST_CAMERA_PERMISSION = 1001
@@ -78,6 +87,7 @@ class DashboardFragment : Fragment() {
             val faceDetectorOptions = FaceDetectorOptions.Builder()
                 .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
                 .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+                .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
                 .build()
             val faceDetector = FaceDetection.getClient(faceDetectorOptions)
 
@@ -96,13 +106,18 @@ class DashboardFragment : Fragment() {
                             faceDetector.process(image)
                                 .addOnSuccessListener { faces ->
                                     for (face in faces) {
+                                        var msg = ""
                                         val bounds = face.boundingBox
+
+                                        calculateDistance(this, face) { distance ->
+                                            msg += "Distance: ${distance * 1000} cm\n"
+
                                         val rotX = face.headEulerAngleX
                                         val rotY =
                                             face.headEulerAngleY // Head is rotated to the right rotY degrees
                                         val rotZ =
                                             face.headEulerAngleZ // Head is tilted sideways rotZ degrees
-                                        var msg = "X: $rotX\nY: $rotY\nZ: $rotZ\n"
+                                            msg += "X: $rotX\nY: $rotY\nZ: $rotZ\n\n"
 
                                         if (face.rightEyeOpenProbability != null) {
                                             val rightEyeOpenProb = face.rightEyeOpenProbability
@@ -113,6 +128,7 @@ class DashboardFragment : Fragment() {
                                             msg += "\nLeft eye open: $leftEyeOpenProb"
                                         }
                                         textViewCameraData.text = msg
+                                        }
                                     }
                                 }
                                 .addOnFailureListener { e ->
@@ -161,5 +177,108 @@ class DashboardFragment : Fragment() {
         } else {
             cameraOn()
         }
+    }
+
+    fun calculateDistance(fragment: Fragment, face: Face, onDistanceCalculated: (Float) -> Unit) {
+        val leftEye = face.getLandmark(FaceLandmark.LEFT_EYE)?.position
+        val rightEye = face.getLandmark(FaceLandmark.RIGHT_EYE)?.position
+
+        // Log the eye positions
+        println("Left Eye: $leftEye, Right Eye: $rightEye")
+
+        if (leftEye != null && rightEye != null) {
+            val eyeDistance = distanceBetweenPoints(leftEye, rightEye)
+
+            // Log the distance between the eyes in pixels
+            println("Eye Distance in Pixels: $eyeDistance")
+
+            // Retrieve the camera info (focal length, sensor width)
+            getCameraInfo(fragment.requireContext(), fragment) { cameraInfo ->
+                if (cameraInfo != null) {
+                    val focalLength = cameraInfo.focalLength
+                    val sensorWidth = cameraInfo.sensorWidth
+
+                    // Log the focal length and sensor width
+                    println("Focal Length: $focalLength, Sensor Width: $sensorWidth")
+
+                    // Known average eye distance in real life (e.g., 6.2 cm)
+                    val knownEyeDistance = 6.2f
+
+                    // Calculate the distance using the formula
+                    val distance = (focalLength * knownEyeDistance) / (eyeDistance * sensorWidth)
+
+                    // Log the calculated distance
+                    println("Calculated Distance: $distance")
+
+                    // Return the calculated distance
+                    onDistanceCalculated(distance)
+                } else {
+                    // Log the failure to retrieve camera info
+                    println("Unable to retrieve camera info.")
+                    onDistanceCalculated(-1f)
+                }
+            }
+        } else {
+            // Log if the eye landmarks are not detected
+            println("Eye landmarks are not detected.")
+            onDistanceCalculated(-1f)
+        }
+    }
+
+    fun distanceBetweenPoints(p1: PointF, p2: PointF): Float {
+        return Math.sqrt(((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y)).toDouble())
+            .toFloat()
+    }
+
+    data class CameraInfo(val focalLength: Float, val sensorWidth: Float)
+
+    fun getCameraInfo(
+        context: Context,
+        lifecycleOwner: Fragment,
+        onCameraInfoAvailable: (CameraInfo?) -> Unit
+    ) {
+        val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
+            ProcessCameraProvider.getInstance(context)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            // Select the back camera
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            // Bind to the lifecycle of the fragment (this)
+            val camera = cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector)
+
+            // Opt-in to Camera2 interop experimental API
+            @OptIn(ExperimentalCamera2Interop::class)
+            val camera2CameraInfo = Camera2CameraInfo.from(camera.cameraInfo)
+
+            // Access the Camera2 characteristics
+            val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+            // Opt-in to Camera2 interop experimental API for accessing the cameraId
+            @OptIn(ExperimentalCamera2Interop::class)
+            val cameraId = camera2CameraInfo.cameraId
+
+            val characteristics = cameraManager.getCameraCharacteristics(cameraId)
+
+            // Get focal length and sensor width
+            val focalLength =
+                characteristics.get(CameraCharacteristics.LENS_INFO_AVAILABLE_FOCAL_LENGTHS)?.get(0)
+            val sensorWidth =
+                characteristics.get(CameraCharacteristics.SENSOR_INFO_PHYSICAL_SIZE)?.width
+
+            // Log retrieved camera info
+            println("Camera Focal Length: $focalLength, Sensor Width: $sensorWidth")
+
+            if (focalLength != null && sensorWidth != null) {
+                onCameraInfoAvailable(CameraInfo(focalLength, sensorWidth))
+            } else {
+                // Log failure to retrieve the info
+                println("Failed to retrieve focal length or sensor width.")
+                onCameraInfoAvailable(null)
+            }
+
+        }, ContextCompat.getMainExecutor(context))
     }
 }
