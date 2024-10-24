eac207a26962217d45976d5f15bce839f2ab56b9
CHONG Fung Yan <230113181@stu.vtc.edu.hk>
2024-10-24 11:01:57 +0800

    add 3 exercise activity

---
+++ iPostureGuard/app/src/main/java/com/example/i_postureguard/ui/exercise/Fragment_exercise_neck.kt
@@ -0,0 +1,26 @@
+package com.example.i_postureguard.ui.exercise
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.i_postureguard.R
+
+class fragment_exercise_neck : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.fragment_exercise_neck)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun onBackButtonClicked(view: View) {
+        finish()
+    }
+}
