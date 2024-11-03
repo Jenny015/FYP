e25daac520cc736ff5b797608b2fc5a021b394e8
LAU Sze Ting (230268178) <230268178@stu.vtc.edu.hk>
2024-11-03 18:49:39 +0800

    Hardcode posture classify, detect every 10 seconds, add voice notification & pop-up window when bad posture detected

--- iPostureGuard/app/src/main/java/com/example/i_postureguard/ui/dashboard/DashboardFragment.kt
+++ iPostureGuard/app/src/main/java/com/example/i_postureguard/ui/dashboard/DashboardFragment.kt
@@ -7,13 +7,17 @@ import android.content.pm.PackageManager
 import android.graphics.PointF
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraManager
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import android.widget.ToggleButton
 import androidx.annotation.OptIn
+import androidx.appcompat.app.AlertDialog
 import androidx.camera.camera2.interop.Camera2CameraInfo
 import androidx.camera.camera2.interop.ExperimentalCamera2Interop
 import androidx.camera.core.CameraSelector
@@ -21,9 +25,11 @@ import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.i_postureguard.R
 import com.example.i_postureguard.databinding.FragmentDashboardBinding
 import com.google.common.util.concurrent.ListenableFuture
 import com.google.mlkit.vision.common.InputImage
@@ -31,6 +37,7 @@ import com.google.mlkit.vision.face.Face
 import com.google.mlkit.vision.face.FaceDetection
 import com.google.mlkit.vision.face.FaceDetectorOptions
 import com.google.mlkit.vision.face.FaceLandmark
+import java.io.File
 
 
 const val REQUEST_CAMERA_PERMISSION = 1001
@@ -38,10 +45,14 @@ class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
     private val binding get() = _binding!!
+    private var accelerometer: GetAccelerometer? = null//initialize GetAccelerometer activity
+    private val detectionDelay = 10000 // For adjust the frequency of detection
+    private lateinit var mediaPlayer: MediaPlayer
 
     //for testing
     private lateinit var textViewCameraData: TextView
-    private var accelerometer: GetAccelerometer? = null//initialize GetAccelerometer activity
+    private var lastUpdateTime = 0L
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -84,6 +95,9 @@ class DashboardFragment : Fragment() {
     @OptIn(ExperimentalGetImage::class)
     private fun cameraOn() {
         textViewCameraData.text = "Camera On"
+        val previewView: PreviewView = binding.cameraPreview
+        previewView.visibility = View.VISIBLE
+
         val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
         cameraProviderFuture.addListener({
             val cameraProvider = cameraProviderFuture.get()
@@ -100,51 +114,67 @@ class DashboardFragment : Fragment() {
                 .build()
                 .also {
                     it.setAnalyzer(ContextCompat.getMainExecutor(requireContext())) { imageProxy ->
-                        val mediaImage = imageProxy.image
-                        if (mediaImage != null) {
-                            val image = InputImage.fromMediaImage(
-                                mediaImage,
-                                imageProxy.imageInfo.rotationDegrees
-                            )
+                        val currentTime = System.currentTimeMillis()
+                        if (currentTime - lastUpdateTime >= detectionDelay) {
+                            lastUpdateTime = currentTime
 
-                            faceDetector.process(image)
-                                .addOnSuccessListener { faces ->
-                                    for (face in faces) {
-                                        var msg = ""
-                                        val bounds = face.boundingBox
+                            val mediaImage = imageProxy.image
+                            if (mediaImage != null) {
+                                val image = InputImage.fromMediaImage(
+                                    mediaImage,
+                                    imageProxy.imageInfo.rotationDegrees
+                                )
 
-                                        calculateDistance(this, face) { distance ->
-                                            msg += "Distance: ${distance * 1000} cm\n"
+                                faceDetector.process(image)
+                                    .addOnSuccessListener { faces ->
+                                        for (face in faces) {
+                                            var msg = ""
+                                            val bounds = face.boundingBox
 
-                                            val rotX = face.headEulerAngleX
-                                            val rotY =
-                                                face.headEulerAngleY // Head is rotated to the right rotY degrees
-                                            val rotZ =
-                                                face.headEulerAngleZ // Head is tilted sideways rotZ degrees
-                                            msg += "X: $rotX\nY: $rotY\nZ: $rotZ\n\n"
+                                            calculateDistance(this, face) { distance ->
+                                                msg += "Distance: ${distance * 1000} cm\n"
 
-                                            if (face.rightEyeOpenProbability != null) {
-                                                val rightEyeOpenProb = face.rightEyeOpenProbability
-                                                msg += "\nRight eye open: $rightEyeOpenProb"
+                                                val rotX = face.headEulerAngleX
+                                                val rotY =
+                                                    face.headEulerAngleY // Head is rotated to the right rotY degrees
+                                                val rotZ =
+                                                    face.headEulerAngleZ // Head is tilted sideways rotZ degrees
+                                                msg += "X: $rotX\nY: $rotY\nZ: $rotZ\n\n"
+
+                                                if (face.rightEyeOpenProbability != null) {
+                                                    msg += "\nRight eye open: ${face.rightEyeOpenProbability}"
+                                                }
+                                                if (face.leftEyeOpenProbability != null) {
+                                                    msg += "\nLeft eye open: ${face.leftEyeOpenProbability}"
+                                                }
+
+                                                // Safely access accelerometer data
+                                                val accelData = getAccelerometerData()
+                                                msg += "\n${accelData}"
+                                                textViewCameraData.text = msg
+
+                                                checkPosture(
+                                                    listOf(rotX, rotY, rotZ),
+                                                    listOf(
+                                                        face.leftEyeOpenProbability,
+                                                        face.rightEyeOpenProbability
+                                                    ), accelData
+                                                )
                                             }
-                                            if (face.leftEyeOpenProbability != null) {
-                                                msg += "\nLeft eye open: ${face.leftEyeOpenProbability}"
-                                            }
-
-                                            // Safely access accelerometer data
-                                            msg += "\n${accelerometer?.getData() ?: "No accelerometer data"}"
-                                            textViewCameraData.text = msg
                                         }
                                     }
-                                }
-                                .addOnFailureListener { e ->
-                                    textViewCameraData.text = "Face detection failed: $e"
-                                }
-                                .addOnCompleteListener {
-                                    imageProxy.close()
-                                }
+                                    .addOnFailureListener { e ->
+                                        textViewCameraData.text = "Face detection failed: $e"
+                                    }
+                                    .addOnCompleteListener {
+                                        imageProxy.close()
+                                    }
+                            }
+                        } else {
+                            imageProxy.close()
                         }
                     }
+
                 }
 
             val preview = Preview.Builder().build().also {
@@ -169,9 +199,11 @@ class DashboardFragment : Fragment() {
             val cameraProvider = cameraProviderFuture.get()
             cameraProvider.unbindAll()
         }, ContextCompat.getMainExecutor(requireContext()))
+        val previewView: PreviewView = binding.cameraPreview
+        previewView.visibility = View.GONE
     }
 
-    fun checkCameraPermission(activity: Activity) {
+    private fun checkCameraPermission(activity: Activity) {
         if (ContextCompat.checkSelfPermission(activity, Manifest.permission.CAMERA)
             != PackageManager.PERMISSION_GRANTED
         ) {
@@ -185,18 +217,13 @@ class DashboardFragment : Fragment() {
         }
     }
 
-    fun calculateDistance(fragment: Fragment, face: Face, onDistanceCalculated: (Float) -> Unit) {
+    private fun calculateDistance(fragment: Fragment, face: Face, onDistanceCalculated: (Float) -> Unit) {
         val leftEye = face.getLandmark(FaceLandmark.LEFT_EYE)?.position
         val rightEye = face.getLandmark(FaceLandmark.RIGHT_EYE)?.position
 
-        // Log the eye positions
-        println("Left Eye: $leftEye, Right Eye: $rightEye")
 
         if (leftEye != null && rightEye != null) {
             val eyeDistance = distanceBetweenPoints(leftEye, rightEye)
-
-            // Log the distance between the eyes in pixels
-            println("Eye Distance in Pixels: $eyeDistance")
 
             // Retrieve the camera info (focal length, sensor width)
             getCameraInfo(fragment.requireContext(), fragment) { cameraInfo ->
@@ -204,41 +231,31 @@ class DashboardFragment : Fragment() {
                     val focalLength = cameraInfo.focalLength
                     val sensorWidth = cameraInfo.sensorWidth
 
-                    // Log the focal length and sensor width
-                    println("Focal Length: $focalLength, Sensor Width: $sensorWidth")
-
                     // Known average eye distance in real life (e.g., 6.2 cm)
                     val knownEyeDistance = 6.2f
 
                     // Calculate the distance using the formula
                     val distance = (focalLength * knownEyeDistance) / (eyeDistance * sensorWidth)
 
-                    // Log the calculated distance
-                    println("Calculated Distance: $distance")
-
                     // Return the calculated distance
                     onDistanceCalculated(distance)
                 } else {
-                    // Log the failure to retrieve camera info
-                    println("Unable to retrieve camera info.")
                     onDistanceCalculated(-1f)
                 }
             }
         } else {
-            // Log if the eye landmarks are not detected
-            println("Eye landmarks are not detected.")
             onDistanceCalculated(-1f)
         }
     }
 
-    fun distanceBetweenPoints(p1: PointF, p2: PointF): Float {
+    private fun distanceBetweenPoints(p1: PointF, p2: PointF): Float {
         return Math.sqrt(((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y)).toDouble())
             .toFloat()
     }
 
     data class CameraInfo(val focalLength: Float, val sensorWidth: Float)
 
-    fun getCameraInfo(
+    private fun getCameraInfo(
         context: Context,
         lifecycleOwner: Fragment,
         onCameraInfoAvailable: (CameraInfo?) -> Unit
@@ -274,17 +291,86 @@ class DashboardFragment : Fragment() {
             val sensorWidth =
                 characteristics.get(CameraCharacteristics.SENSOR_INFO_PHYSICAL_SIZE)?.width
 
-            // Log retrieved camera info
-            println("Camera Focal Length: $focalLength, Sensor Width: $sensorWidth")
-
             if (focalLength != null && sensorWidth != null) {
                 onCameraInfoAvailable(CameraInfo(focalLength, sensorWidth))
             } else {
-                // Log failure to retrieve the info
-                println("Failed to retrieve focal length or sensor width.")
                 onCameraInfoAvailable(null)
             }
 
         }, ContextCompat.getMainExecutor(context))
+    }
+
+    private fun getAccelerometerData(): String {
+        return accelerometer?.getData() ?: "No accelerometer data"
+    }
+
+    fun collectData(cameraData: List<Float>, accelData: List<Float>) {
+        var label = -1
+        val combinedData = accelData + cameraData + listOf(label)
+        val dataLine = combinedData.joinToString(separator = ",")
+        val file = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+            "posture_data.csv"
+        )
+        file.appendText("$dataLine\n")
+    }
+
+    private fun checkPosture(head: List<Float>, eyeopen: List<Float?>, accelerometer: String) {
+        Toast.makeText(this.requireContext(), "For dev: Detect", Toast.LENGTH_SHORT).show() //For Testing
+        val buffer = 0 //TODO: Access from user profile
+        // Phrase accelerometer data from String into List
+        val regex = """[-\d]+\.\d+""".toRegex()
+        val accelData =
+            regex.findAll(accelerometer).map { it.value.trim() }.mapNotNull { it.toFloatOrNull() }
+                .toMutableList()
+        // TODO: Calculate actual head angle with accelerometer data
+//            collectData(head, accelData);
+        // TODO: Set kinds of bad posture will be detected
+        //Head position: Tilt Front (Text-neck), Tilt left(scoliosis), Tile Right(scoliosis)
+        //Phone position: Phone horizontal & Head horizontal (Sleep on side with phone), Phone face down with face detected (Sleep on back with phone)
+        // TODO: Set threshold for different kinds of bad posture
+        var msg = ""
+        if (head[2] > -20 && head[2] < 20 && (accelData[0] > 7 || accelData[0] < -7)) {
+            playAudio(R.raw.sleep)
+            msg += "Sleep on side while using mobile phone\n"
+        } else if (accelData[2] < -7) {
+            playAudio(R.raw.sleep)
+            msg += "Sleep on back while using mobile phone\n"
+        } else {
+            if (accelData[2] < 5 && head[0] < (3 - buffer) || (accelData[2] > 5 && head[0] < (12 - buffer))) {
+                playAudio(R.raw.text_neck)
+                msg += "Text-neck posture\n"
+            }
+            if (head[2] < (-10 - buffer)) {
+                playAudio(R.raw.tilt_left)
+                msg += "Head tilting left (Scoliosis)\n"
+            }
+            if (head[2] > (10 + buffer)) {
+                playAudio(R.raw.tilt_right)
+                msg += "Head tilting right (Scoliosis)\n"
+            }
+        }
+        if(msg.isNotEmpty()){
+            showDialog(msg)
+        }
+    }
+
+    private fun playAudio(resId: Int) {
+        if (::mediaPlayer.isInitialized) {
+            mediaPlayer.release()
+        }
+
+        mediaPlayer = MediaPlayer.create(this.requireContext(), resId)
+        mediaPlayer.start()
+    }
+    private fun showDialog(msg: String){
+            val builder = AlertDialog.Builder(this.requireContext())
+            builder.setTitle("Posture Reminder")
+            builder.setMessage(msg)
+            builder.setPositiveButton("OK") { dialog, which ->
+                dialog.dismiss()
+            }
+            val dialog = builder.create()
+            dialog.show()
     }
 }
